c8b6126e98b901ec4e88f2964355a4ae8e380fc6
dkhromova <d.khromova47@gmail.com>
2025-11-05 15:49:35 +0600

    Initial commit: Аттестация по веб-тестированию

---
+++ src/test/java/exam/tests/LoginTests.java
@@ -0,0 +1,54 @@
+package exam.tests;
+
+import exam.base.BasePage;
+import exam.pageObjects.LoginPage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+public class LoginTests extends BasePage {
+    LoginPage loginPage = new LoginPage();
+
+    @DisplayName("Успешная авторизация")
+    @Story("Как пользователь я могу авторизоваться в магазине")
+    @Description("Авторизация по корректным кредам не заблокированного пользователя")
+    @Tags({@Tag("Позитивный"), @Tag("Смоук")})
+    @Test
+    void successfulLoginTest() {
+        loginPage
+                .setUsername("standard_user")
+                .setPassword("secret_sauce")
+                .clickLoginSuccess()
+                .checkIsOpened();
+    }
+
+    @DisplayName("Авторизация заблокированного пользователя")
+    @Story("Как заблокированный пользователь я не могу авторизоваться в магазине")
+    @Description("Авторизация по заблокированным кредам пользователя")
+    @Tags({@Tag("Позитивный"), @Tag("Смоук")})
+    @Test
+    void blockedUserLoginTest() {
+        loginPage
+                .setUsername("locked_out_user")
+                .setPassword("secret_sauce")
+                .clickLoginFail()  // используем метод для неуспешного логина
+                .checkError("Epic sadface: Sorry, this user has been locked out.");
+    }
+
+    @DisplayName("Авторизация замедленного пользователя")
+    @Story("Как пользователь с плохим интернетом я могу авторизоваться в магазине")
+    @Description("Авторизация по корректным кредам пользователя с задержкой")
+    @Tags({@Tag("Позитивный"), @Tag("Смоук")})
+    @Test
+    void performanceGlitchUserLoginTest() {
+        loginPage
+                .setUsername("performance_glitch_user")
+                .setPassword("secret_sauce")
+                .clickLoginSuccess()
+                .checkIsOpened();
+    }
+
+}
